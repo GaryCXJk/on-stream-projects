fc702aad4f3e371dc2b87b831e0034a512cd668f
BuildTools <unconfigured@null.spigotmc.org>
2023-07-21 22:54:00 +0200

    Improved Songify widget

--- songify/songify.js
+++ songify/songify.js
@@ -4,6 +4,7 @@ const moveSpeed = .2;
 
 const imageElement = document.querySelector('#image');
 const progressElement = document.querySelector('#progress');
+const timeElement = document.querySelector('#time');
 
 const screens = {
     loading: document.querySelector('#loading'),
@@ -12,7 +13,7 @@ const screens = {
 };
 
 let songId = -1;
-let progress = 0;
+let progress = [0, 0, 0, false];
 
 const info = {
     artist: {
@@ -25,6 +26,16 @@ const info = {
     },
     requested: {
         element: document.querySelector('#requested'),
+        ticker: 0,
+    },
+    next: {
+        element: document.querySelector('#next'),
+        none: document.querySelector('#next-none'),
+        available: document.querySelector('#next-available'),
+        artist: document.querySelector('#next-artist'),
+        song: document.querySelector('#next-song'),
+        duration: document.querySelector('#next-duration'),
+        requested: document.querySelector('#next-requested'),
         ticker: 0,
     }
 }
@@ -42,6 +53,7 @@ function setSong(data) {
         info.artist.ticker = 0;
         info.title.ticker = 0;
         info.requested.ticker = 0;
+        info.next.ticker = 0;
 
         if (songId) {
             switchScreen('content');
@@ -52,21 +64,54 @@ function setSong(data) {
             info.artist.element.textContent = data?.Artists ?? '';
             info.title.element.textContent = data?.Title ?? '';
             info.requested.element.textContent = data?.Requester ? `@${data?.Requester}` : 'None';
+
+            let nextSong = null;
+            if (data?.QueueCount) {
+                let queueIndex = 0;
+                while (queueIndex < data.QueueCount && data.Queue[queueIndex].trackid === songId) {
+                    queueIndex++;
+                }
+                if (queueIndex < data.QueueCount) {
+                    nextSong = data.Queue[queueIndex];
+                }
+            }
+
+            if (nextSong) {
+                const {
+                    artist,
+                    title,
+                    length,
+                    requester,
+                } = nextSong;
+                info.next.none.classList.add('hidden');
+                info.next.available.classList.remove('hidden');
+
+                info.next.artist.textContent = artist;
+                info.next.song.textContent = title;
+                info.next.duration.textContent = length;
+                info.next.requested.textContent = requester;
+            } else {
+                info.next.none.classList.remove('hidden');
+                info.next.available.classList.add('hidden');
+            }
         } else {
             imageElement.style.backgroundImage = 'url(\'./music-note.svg\')';
             switchScreen('nothing');
         }
     }
-    progress = data.DurationPercentage;
+    if (progress[0] !== data.Progress || progress[3] !== data.IsPlaying) {
+        progress = [data.Progress ?? 0, data.DurationTotal ?? 0, Date.now(), data.IsPlaying];
+    }
 }
 
 async function reloadData() {
+    const dateStart = Date.now();
     const response = await fetch(apiUrl);
     const data = await response.json();
 
     setSong(data);
 
-    setTimeout(reloadData, 500);
+    setTimeout(reloadData, Math.max(0, 1000 - (Date.now() - dateStart)));
 }
 
 function animateElement(type) {
@@ -99,13 +144,37 @@ function animateElement(type) {
     }
 }
 
+function parseTime(timeValue) {
+    const minutes = Math.floor(timeValue / 60000);
+    const seconds = Math.floor(timeValue / 1000) % 60;
+    
+    return `${`${minutes}`.padStart(2, '0')}:${`${seconds}`.padStart(2, '0')}`;
+}
+
+function animateTime() {
+    let currentTime = progress[0];
+    let bar = 0;
+    // if (progress[3]) {
+    //     currentTime += (Date.now() - progress[2]);
+    // }
+    if (progress[1] === 0) {
+        timeElement.textContent = '--:-- / --:--';
+    } else {
+        timeElement.textContent = `${parseTime(currentTime)} / ${parseTime(progress[1])}`;
+        
+        bar = (currentTime / progress[1]) * 100;
+    }
+    progressElement.style.width = `${bar}%`;
+    
+    requestAnimationFrame(animateTime);
+}
+
 function animate() {
     if (songId) {
         animateElement('artist');
         animateElement('title');
         animateElement('requested');
-
-        progressElement.style.width = `${progress}%`;
+        animateElement('next');
     }
 
     requestAnimationFrame(animate);
@@ -114,3 +183,4 @@ function animate() {
 reloadData();
 
 animate();
+animateTime();
